ed42316c2326900d99e72810eac1f002ba59908b
Sahil9914 <sahilchalotra71@gmail.com>
2025-10-07 00:19:09 +0530

    Update web.py

--- web.py
+++ web.py
@@ -184,7 +184,7 @@ def recommend(movie):
 # Load your data
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
 
 # Initialize usage tracking
 init_usage_tracking()
